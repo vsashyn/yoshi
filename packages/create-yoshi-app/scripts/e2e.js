226c8871fe876d557357acd020305620eb3cd6f0
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-11-18 18:34:33 +0200

    debug ci

--- packages/create-yoshi-app/scripts/e2e.js
+++ packages/create-yoshi-app/scripts/e2e.js
@@ -14,10 +14,10 @@ const {
 const {
   killSpawnProcessAndHisChildren,
 } = require('../../../test-helpers/process');
-const { isTeamCity } = require('yoshi-helpers');
+const { inTeamCity } = require('yoshi-helpers');
 
 // verbose logs and output
-const verbose = process.env.VERBOSE_TESTS || isTeamCity();
+const verbose = process.env.VERBOSE_TESTS || inTeamCity();
 // A regex pattern to run a focus test on the matched projects types
 const focusProjectPattern = 'fullstack-typescript'; //process.env.FOCUS_PATTERN;
 
